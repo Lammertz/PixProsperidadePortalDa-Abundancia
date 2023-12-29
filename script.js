25327c8999adbcd93c07c2071637ea176fa122cd
sitePIX <39447969+Lammertz@users.noreply.github.com>
2023-12-29 13:52:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,48 @@ document.addEventListener('click', function() {
     audio.play();
     document.removeEventListener('click', arguments.callee);
 });
+
+// Array de mensagens positivas
+const positiveMessages = [
+    "Você é capaz de atrair prosperidade!",
+    "A abundância está a caminho!",
+    "Cada contribuição é uma semente de sucesso!",
+    "Visualize a riqueza em sua vida!",
+    "O universo conspira a seu favor!",
+    "Sua positividade cria ondas de sucesso!",
+    "Acredite na magia do pensamento positivo!",
+    "Seu futuro financeiro começa agora!",
+    "Sinta a energia positiva fluindo em sua vida!",
+    "Prospere com gratidão e alegria!"
+];
+
+// Função para exibir mensagens aleatórias em um loop infinito
+function showRandomMessages() {
+    const messagesContainer = document.getElementById("positiveMessagesContainer");
+
+    // Limpa mensagens existentes
+    messagesContainer.innerHTML = '';
+
+    // Exibe uma mensagem aleatória por vez
+    let index = 0;
+    function displayNextMessage() {
+        const messageElement = document.createElement("div");
+        messageElement.className = "positive-message";
+        messageElement.textContent = positiveMessages[index];
+
+        messagesContainer.appendChild(messageElement);
+
+        // Define um tempo para a mensagem desaparecer (por exemplo, 5 segundos)
+        setTimeout(() => {
+            messageElement.remove();
+            index = (index + 1) % positiveMessages.length;
+            setTimeout(displayNextMessage, 3000); // Aguarda 3 segundos antes de chamar a próxima mensagem
+        }, 5000); // Ajuste conforme necessário
+    }
+
+    // Inicia o loop de exibição de mensagens
+    displayNextMessage();
+}
+
+// Chama a função para exibir mensagens aleatórias em um loop infinito
+showRandomMessages();
